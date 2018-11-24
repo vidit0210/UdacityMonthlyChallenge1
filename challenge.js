6703390ebba2c912d28bad7d0d0fa21d6007a900
Vidit Shah <vidit0210@gmail.com>
2018-11-24 19:39:28 +0530

    Initial Commit

---
+++ challenge.js
@@ -0,0 +1,11 @@
+//Author Vidit Shah
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ||3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT,()=>{console.log(`Listening on port ${PORT} `)});
+
